2100af47d4fe4ef0ea3d7e3c6767c7a760b82b4b
Walter Hannah <hannah6@llnl.gov>
2025-12-11 08:19:04 -0800

    update code_grid_plot/plot.grid.scrip.v2.py

--- code_grid_plot/plot.grid.scrip.v2.py
+++ code_grid_plot/plot.grid.scrip.v2.py
@@ -29,10 +29,10 @@ grid_root = '/global/cfs/cdirs/e3sm/2026-INCITE-CONUS-RRM/files_grid'
 add_grid(f'{grid_root}/2026-incite-conus-1024x4-pg2_scrip.nc','conus-1024x4', clat=40,clon=360-105)
 num_plot_col = 1
 
-
-# dx_min,dx_max = 2.0,45.0 # good for ne128 RRM
-dx_min,dx_max = 0.2, 6.0 # good for ne1024 RRM
-
+#---------------------------------------------------------------------------------------------------
+# set explicit bounds for colorbar - or comment out to have it determined automatically
+# dx_min,dx_max = 2.0,45.0; use_log_color = True # good for ne128 RRM
+dx_min,dx_max = 0.2, 6.0; use_log_color = True # good for ne1024 RRM
 #---------------------------------------------------------------------------------------------------
 
 fig_file,fig_type = f'{home}/E3SM_grid_support/figs_grid_plot/grid.scrip.v2','png'
@@ -83,14 +83,15 @@ res.lbLabelFontHeightF          = 0.008
 res.lbLabelBarOn                = True
 res.lbTitleString               = 'approx. grid spacing [km]'
 res.cnFillMode                  = 'CellFill'
-# res.mpGridAndLimbOn             = True
+res.mpGridAndLimbOn             = True
 # res.mpOutlineBoundarySets       = 'NoBoundaries'
-# res.mpOutlineBoundarySets       = 'GeophysicalAndUSStates'
-# res.mpPerimOn                   = False
-# res.pmTickMarkDisplayMode       = 'Never'
-# res.mpGeophysicalLineColor      = 'white'
-# res.mpGeophysicalLineThicknessF = 6
+res.mpOutlineBoundarySets       = 'GeophysicalAndUSStates'
+res.mpPerimOn                   = False
+res.pmTickMarkDisplayMode       = 'Never'
+res.mpGeophysicalLineColor      = 'white'
+res.mpGeophysicalLineThicknessF = 6
 # res.mpDataBaseVersion           = 'MediumRes' # LowRes / MediumRes / HighRes
+# res.mpProjection = 'Robinson'
 
 # res.mpLimitMode     = 'LatLon'
 # res.mpMinLatF,res.mpMaxLatF,res.mpMinLonF,res.mpMaxLonF =  0, 60, -150, -45 # CONUS
@@ -103,36 +104,36 @@ mres.xyMarkerSizeF = 0.008
 #---------------------------------------------------------------------------------------------------
 # define function to add subtitles to the top of plot
 #---------------------------------------------------------------------------------------------------
-def set_subtitles(wks, plot, left_string='', center_string='', right_string='', font_height=0.01):
+def set_subtitles(wks, plot, lstr='', cstr='', rstr='', font_height=0.01):
   ttres         = ngl.Resources()
   ttres.nglDraw = False
-
-  ### Use plot extent to call ngl.text(), otherwise you will see this error:
-  ### GKS ERROR NUMBER   51 ISSUED FROM SUBROUTINE GSVP  : --RECTANGLE DEFINITION IS INVALID
+  # Use plot extent to call ngl.text(), otherwise you will see this error:
+  # GKS ERROR NUMBER   51 ISSUED FROM SUBROUTINE GSVP  : --RECTANGLE DEFINITION IS INVALID
   strx = ngl.get_float(plot,'trXMinF')
   stry = ngl.get_float(plot,'trYMinF')
   ttres.txFontHeightF = font_height
-
-  ### Set annotation resources to describe how close text is to be attached to plot
+  #-----------------------------------------------------------------------------
+  # Set annotation resources to describe how close text is to be attached to plot
   amres = ngl.Resources()
   if not hasattr(ttres,'amOrthogonalPosF'):
     amres.amOrthogonalPosF = -0.52   # Top of plot plus a little extra to stay off the border
   else:
     amres.amOrthogonalPosF = ttres.amOrthogonalPosF
-
-  ### Add left string
+  #-----------------------------------------------------------------------------
+  # Add left string
   amres.amJust,amres.amParallelPosF = 'BottomLeft', -0.5   # Left-justified
-  tx_id_l   = ngl.text(wks, plot, left_string, strx, stry, ttres)
+  tx_id_l   = ngl.text(wks, plot, lstr, strx, stry, ttres)
   anno_id_l = ngl.add_annotation(plot, tx_id_l, amres)
+  #-----------------------------------------------------------------------------
   # Add center string
   amres.amJust,amres.amParallelPosF = 'BottomCenter', 0.0   # Centered
-  tx_id_c   = ngl.text(wks, plot, center_string, strx, stry, ttres)
+  tx_id_c   = ngl.text(wks, plot, cstr, strx, stry, ttres)
   anno_id_c = ngl.add_annotation(plot, tx_id_c, amres)
+  #-----------------------------------------------------------------------------
   # Add right string
   amres.amJust,amres.amParallelPosF = 'BottomRight', 0.5   # Right-justified
-  tx_id_r   = ngl.text(wks, plot, right_string, strx, stry, ttres)
+  tx_id_r   = ngl.text(wks, plot, rstr, strx, stry, ttres)
   anno_id_r = ngl.add_annotation(plot, tx_id_r, amres)
-
   return
 #---------------------------------------------------------------------------------------------------
 # Load data and create plot
@@ -151,7 +152,6 @@ if 'dx_min' not in locals() and 'dx_max' not in locals():
   for f in range(num_grid):
     dx_min = np.min([dx_min,np.nanmin(grid_data_list[f])])
     dx_max = np.max([dx_max,np.nanmax(grid_data_list[f])])
-
 #---------------------------------------------------------------------------------------------------
 print()
 print(f' dx_min: {dx_min}')
@@ -166,27 +166,24 @@ for f in range(num_grid):
   #-----------------------------------------------------------------------------
   num_grid_cols[f] = len(ds_grid['grid_area'])
   #-----------------------------------------------------------------------------
-  # ds_grid['grid_center_lon'] = ds_grid['grid_center_lon'].assign_attrs(units='degrees_east')
-  # ds_grid['grid_center_lat'] = ds_grid['grid_center_lat'].assign_attrs(units='degrees_north')
-  # ds_grid['grid_corner_lon'] = ds_grid['grid_corner_lon'].assign_attrs(units='degrees_east')
-  # ds_grid['grid_corner_lat'] = ds_grid['grid_corner_lat'].assign_attrs(units='degrees_north')
-  #-----------------------------------------------------------------------------
   tres = copy.deepcopy(res)  
   tres.sfXArray      = ds_grid['grid_center_lon'].values
   tres.sfYArray      = ds_grid['grid_center_lat'].values
   tres.sfXCellBounds = ds_grid['grid_corner_lon'].values
   tres.sfYCellBounds = ds_grid['grid_corner_lat'].values
-
+  #-----------------------------------------------------------------------------
   tres.cnFillPalette = np.array( cmocean.cm.curl(np.linspace(0,1,256)) )
   tres.cnLevelSelectionMode = "ExplicitLevels"
-  # tres.cnLevels = np.linspace(dx_min,dx_max,num=60)
-  tres.cnLevels = np.logspace(np.log10(dx_min),np.log10(dx_max),num=60)
-
-  # tres.mpProjection = 'Robinson'
+  if use_log_color:
+    # log scaling works best for RRM
+    tres.cnLevels = np.logspace(np.log10(dx_min),np.log10(dx_max),num=60)
+  else:
+    tres.cnLevels = np.linspace(dx_min,dx_max,num=60)
+  #-----------------------------------------------------------------------------
   tres.mpProjection = 'Orthographic'
   tres.mpCenterLonF = clon_list[f]
   tres.mpCenterLatF = clat_list[f]
-
+  #-----------------------------------------------------------------------------
   plot[f] = ngl.contour_map(wks, topo, tres)
 
   #-----------------------------------------------------------------------------
