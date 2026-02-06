1e73cbaf1efbba4c7dd62b2b0a0224667dc3ef33
Walter Hannah <hannah6@llnl.gov>
2026-02-06 10:18:14 -0600

    update set_project_paths.sh

--- set_project_paths.sh
+++ set_project_paths.sh
@@ -9,6 +9,14 @@ for arg in "$@"; do
   esac
 done
 #-------------------------------------------------------------------------------
+# ANSI color codes for highlighting terminal output
+export RED='\033[0;31m'
+export GRN='\033[0;32m'
+export YLW='\e[33m'
+export CYN='\033[0;36m'
+export BLD='\033[1m'
+export NC='\033[0m'
+#-------------------------------------------------------------------------------
 export proj_root=${grid_code_root}/${proj}
 export data_root=${grid_data_root}/${proj}
 export slurm_log_root=${proj_root}/logs_slurm
@@ -24,37 +32,36 @@ export domn_root=${data_root}/files_domain
 export homme_tool_root=${e3sm_src_root}/cmake_homme
 # export homme_tool_root=$SCRATCH/hommetool
 #-------------------------------------------------------------------------------
-echo --------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-echo "MACHINE SPECIFIC PATHS:"
-echo "  host              = $host"
-echo "  grid_code_root    = $grid_code_root"
-echo "  grid_data_root    = $grid_data_root"
-echo "  e3sm_src_root     = $e3sm_src_root"
-echo "  homme_tool_root   = ${homme_tool_root}"
-echo "  DIN_LOC_ROOT      = $DIN_LOC_ROOT"; echo
-echo "  unified_src       = $unified_src"
-echo "  unified_bin       = $unified_bin"; echo
-echo "  topo_file_src     = $topo_file_src"
-echo "  mbda_path         = $mbda_path"; echo
-#-------------------------------------------------------------------------------
-echo "PROJECT SPECIFIC PATHS:"
-echo "  proj              = ${proj}"
-echo "  data_root         = ${data_root}"; echo
-echo "  grid_root         = ${grid_root}"
-echo "  maps_root         = ${maps_root}"
-echo "  topo_root         = ${topo_root}"
-echo "  init_root         = ${init_root}"
-echo "  domn_root         = ${domn_root}"; echo
-# echo "  atms_root         = ${atms_root}"; echo
-echo "  slurm_log_root    = ${slurm_log_root}"
-echo "  hiccup_log_root   = ${hiccup_log_root}"; echo
-#-------------------------------------------------------------------------------
-echo "SLURM VARIABLES:"
-echo "  slurm_mail_user   = $slurm_mail_user"
-echo "  slurm_mail_type   = $slurm_mail_type"; echo
-#-------------------------------------------------------------------------------
-echo --------------------------------------------------------------------------------
+echo    "--------------------------------------------------------------------------------"
+echo -e "${BLD}MACHINE SPECIFIC PATHS:${NC}"
+echo    "  host              = $host"
+echo    "  grid_code_root    = $grid_code_root"
+echo    "  grid_data_root    = $grid_data_root"
+echo    "  e3sm_src_root     = $e3sm_src_root"
+echo    "  homme_tool_root   = ${homme_tool_root}"
+echo    "  DIN_LOC_ROOT      = $DIN_LOC_ROOT"; echo
+echo    "  unified_src       = $unified_src"
+echo    "  unified_bin       = $unified_bin"; echo
+echo    "  topo_file_src     = $topo_file_src"
+echo    "  mbda_path         = $mbda_path"
+echo    "--------------------------------------------------------------------------------"
+echo -e "${BLD}PROJECT SPECIFIC PATHS:${NC}"
+echo    "  proj              = ${proj}"
+echo    "  timestamp         = ${timestamp}"; echo
+echo    "  data_root         = ${data_root}"
+echo    "  grid_root         = ${grid_root}"
+echo    "  maps_root         = ${maps_root}"
+echo    "  topo_root         = ${topo_root}"
+echo    "  init_root         = ${init_root}"
+echo    "  domn_root         = ${domn_root}"; echo
+# echo    "  atms_root         = ${atms_root}"; echo
+echo    "  slurm_log_root    = ${slurm_log_root}"
+echo    "  hiccup_log_root   = ${hiccup_log_root}"
+echo    "--------------------------------------------------------------------------------"
+echo -e "${BLD}USER SPECIFIC SLURM VARIABLES:${NC}"
+echo    "  slurm_mail_user   = $slurm_mail_user"
+echo    "  slurm_mail_type   = $slurm_mail_type"
+echo    "--------------------------------------------------------------------------------"
 #-------------------------------------------------------------------------------
 if ! $skip_mkdir; then
   if [ ! -d ${grid_root} ]; then mkdir -p ${grid_root}; fi
@@ -68,11 +75,4 @@ if ! $skip_mkdir; then
 fi
 #-------------------------------------------------------------------------------
 ulimit -s unlimited # required for larger grids
-#-------------------------------------------------------------------------------
-# ANSI color codes for highlighting terminal output
-export RED='\033[0;31m'
-export GRN='\033[0;32m'
-export YLW='\e[33m'
-export CYN='\033[0;36m'
-export NC='\033[0m'
 #-------------------------------------------------------------------------------
