93961b2138ce22152e5ad12cc69766b484280146
Meng Huang <meng.huang@pnnl.gov>
2026-02-24 14:13:38 -0800

    Patch for 2026-workflow-test

--- 2026-workflow-test/set_project.sh
+++ 2026-workflow-test/set_project.sh
@@ -2,7 +2,7 @@
 #-------------------------------------------------------------------------------
 # proj="2026-ne30-test" # project "name" or tag that will be used for data roo path
 proj="2026-workflow-test" # project "name" or tag that will be used for data roo path
-export timestamp="20260204" # yyyymmdd - timestamp for all files (just use the date you started)  
+export timestamp=$(date +"%Y%m%d") # yyyymmdd - timestamp for all files (just use the date you started)  
 #-------------------------------------------------------------------------------
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${SCRIPT_DIR}/../set_machine_paths.sh
